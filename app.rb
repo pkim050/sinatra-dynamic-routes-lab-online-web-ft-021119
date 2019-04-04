73fa59fd01af0b785250334aef7423685ef981b8
Patrick Kim <pkim050@gmail.com>
2019-04-04 09:35:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,8 +16,10 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-
-    "#{@phrase} #{@phrase}"
+    @number.times do
+      @phrase += " #{@phrase}"
+    end
+    "#{@phrase}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
