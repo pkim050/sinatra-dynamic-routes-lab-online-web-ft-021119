8e616643926c74e3e0da4f8d4e8e6eeb12501724
Patrick Kim <pkim050@gmail.com>
2019-04-04 09:44:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,8 +25,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    #@total = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
-    #{}"#{@total}"
+    @total = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
+    "#{@total}"
   end
 
   get '/:operation/:number1/:number2' do
